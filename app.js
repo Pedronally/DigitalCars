f94e59d1f4f0a9bd57d6988f00fff73de2a7f484
Pedro Nally <Pedronally05@gmail.com>
2022-05-19 20:22:15 -0300

    server launcher

---
+++ app.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const path = require("path");
+const app = express();
+
+
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, './views/home.html'))
+});
+
+app.listen(3050, () => console.log("")); 
+
+app.use(express.static('public'));
