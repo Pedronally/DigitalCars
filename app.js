40d24edd7241734c60578db33959decc1142bc99
Pedro Nally <Pedronally05@gmail.com>
2022-06-01 19:15:19 -0300

    v2

--- app.js
+++ app.js
@@ -2,12 +2,33 @@ const express = require("express");
 const path = require("path");
 const app = express();
 
+app.use(express.static('public'));
 
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, './views/index.html'))
 });
 
+
+app.get('/detalleDeProducto', (req, res) => {
+    res.sendFile(path.join(__dirname, './views/detalleDeProducto.html'))
+});
+
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, './views/.html'))
+});
+
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, './views/.html'))
+});
+
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, './views/.html'))
+});
+
 app.listen(3050, () => console.log("Tamo Activo")); 
 
-app.use(express.static('public'));
+
