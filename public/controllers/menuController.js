3247d8d64ccb18883d120596fd5f93c549f0bc81
ApiPeralta <juanpabloperaltapino@gmail.com>
2022-06-16 21:07:17 -0300

    Empezando controllers

---
+++ public/controllers/menuController.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const path = require("path");
+
+const controlador = {
+    index: (req, res) => {
+        res.sendFile(path.join(__dirname, './views/index.html'))
+    },
+    login: (req, res) => {
+        res.sendFile(path.join(__dirname, './views/login.html'))
+    },
+    register: (req, res) => {
+        res.sendFile(path.join(__dirname, './views/register.html'))
+    },
+    carrito: (req, res) => {
+        res.sendFile(path.join(__dirname, './views/carrito.html'))
+    },
+    detalle: (req, res) => {
+        res.sendFile(path.join(__dirname, './views/detalleDeProducto.html'))
+    },
+};
+
+module.exports = controlador;
