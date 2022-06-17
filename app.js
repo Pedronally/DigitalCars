ba027f0aaf5825990ab1588078beb47609dedb5a
ApiPeralta <juanpabloperaltapino@gmail.com>
2022-06-16 21:37:24 -0300

    Empezando controllers y routers

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const router = require('./public/routes/mainRouter');
 const path = require("path");
 const app = express();
 
@@ -6,29 +7,7 @@ app.set('view engine', 'ejs');
 
 app.use(express.static('public'));
 
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, './views/index.html'))
-});
-
-app.get('/index', (req, res) => {
-    res.sendFile(path.join(__dirname, './views/index.html'))
-});
-
-app.get('/detalleDeProducto', (req, res) => {
-    res.sendFile(path.join(__dirname, './views/detalleDeProducto.html'))
-});
-
-app.get('/login', (req, res) => {
-    res.sendFile(path.join(__dirname, './views/login.html'))
-});
-
-app.get('/carrito', (req, res) => {
-    res.sendFile(path.join(__dirname, './views/carrito.html'))
-});
-
-app.get('/register', (req, res) => {
-    res.sendFile(path.join(__dirname, './views/register.html'))
-});
+app.use('/index',router);
 
 
 
---
+++ public/routes/mainRouter.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const controller = require('../controllers/menuController');
+
+const router = express.Router();
+
+router.get('/index', controller.index);
+router.get('/login', controller.login);
+router.get('/register', controller.register);
+router.get('/carrito', controller.carrito);
+router.get('/detalle', controller.detalle);
+
+module.exports = router;
