1d3f724a6eb4f2f7dfe85633be48422ca8a330b4
FrancoVer12 <flvernengo@gmail.com>
2022-08-10 20:50:47 -0300

    vista editar dinamica implementada

--- src/controllers/adminController.js
+++ src/controllers/adminController.js
@@ -1,10 +1,18 @@
 const path = require("path");
+const fs = require('fs');
+const productArchivo = fs.readFileSync (path.join(__dirname, '../data/productos.json'))
+
 
 const controller = {
 
     createProduct: (req, res) => {
         res.render('crearProducto')
     },
+    editProduct: (req, res) => {
+        const products = JSON.parse(productArchivo)
+        const auto = products.find(auto => auto.id == req.params.id)
 
+        res.render('editarProducto', {auto})
+    },
 }
 module.exports = controller
--- src/routes/adminRouter.js
+++ src/routes/adminRouter.js
@@ -3,5 +3,6 @@ const router = express.Router();
 const adminController = require('../controllers/adminController');
 
 router.get('/create', adminController.createProduct)
+router.get('/edit/:id', adminController.editProduct)
 
 module.exports = router;
