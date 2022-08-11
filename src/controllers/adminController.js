370a0ecd0e4ee5244bb4b5de155094653be4937a
FrancoVer12 <flvernengo@gmail.com>
2022-08-10 21:46:52 -0300

    guardado correcto de productos nuevos

--- src/controllers/adminController.js
+++ src/controllers/adminController.js
@@ -14,5 +14,23 @@ const controller = {
 
         res.render('editarProducto', {auto})
     },
+    saveNew: (req, res) => {
+        const products = JSON.parse(productArchivo)
+        const ultimoAuto = products.pop()
+        let autoNuevo = {
+            id: ultimoAuto.id +1,
+            modelo: req.body.modelo,
+            precio: req.body.precio,
+            color: req.body.color,
+            motor: req.body.motor,
+        } 
+        products.push(autoNuevo)
+        const nuevoJson = JSON.stringify(products)
+        fs.writeFileSync (path.join(__dirname, '../data/productos.json'), nuevoJson)
+        res.render('crearProducto')
+    },
+    saveEdit: (req, res) => {
+        res.render('crearProducto')
+    },
 }
 module.exports = controller
--- src/routes/adminRouter.js
+++ src/routes/adminRouter.js
@@ -4,5 +4,8 @@ const adminController = require('../controllers/adminController');
 
 router.get('/create', adminController.createProduct)
 router.get('/edit/:id', adminController.editProduct)
+router.put('/edit/:id', adminController.saveEdit)
+router.post('/create', adminController.saveNew)
+
 
 module.exports = router;
