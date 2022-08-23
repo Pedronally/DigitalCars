1ab224ff40b95689a8369df316ecfbc13eebeb50
Matias <matia@LAPTOP-HUGO76BI>
2022-08-23 11:14:02 -0300

    .

--- src/database/config/config.js
+++ src/database/config/config.js
@@ -2,7 +2,7 @@ module.exports = {
     "development": {
       "username": "root",
       "password": '',
-      "database": "digitalCars_db",
+      "database": "digitalCars",
       "host": "127.0.0.1",
       "dialect": "mysql"
     },
